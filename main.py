008e661c35285c40e045ef9c4d4e735b4b5ebd59
dosdogruyup <berk-dmrdzn@hotmail.com>
2026-01-02 01:38:10 +0300

    tamamlandı

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from stats import get_num_words, get_num_chars
+import sys
+from stats import get_num_words, get_num_chars, to_listed_dicts
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
+
+def print_char_count(dict_list):
+    for d in dict_list: 
+        if d["char"].isalpha():
+            print(f"{d["char"]}: {d["num"]}")
+
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    book_loc = ""
+    if len(sys.argv) == 2:
+        book_loc = sys.argv[1]
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    print("============ BOOKBOT ============")
+    
+    print(f"Analyzing book found at {book_loc}")
+    book_text = get_book_text(book_loc)
+    
+    print("----------- Word Count ----------")
     print(f"Found {get_num_words(book_text)} total words")
-    print(get_num_chars(book_text))
+    
+    print("--------- Character Count -------")
+    print_char_count(to_listed_dicts(get_num_chars(book_text)))
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,6 @@
 def get_num_words(text):
     return len(text.split())
+
 def get_num_chars(text):
     text = text.lower()
     char_dict = {}
@@ -9,3 +10,13 @@ def get_num_chars(text):
         else:
             char_dict[c] = 1
     return char_dict
+
+def helper_func(dict_list):
+    return dict_list["num"]
+
+def to_listed_dicts(char_dict):
+    dict_list = []
+    for k in char_dict:
+        dict_list.append({"char": k, "num": char_dict[k]})
+    dict_list.sort(key = helper_func, reverse = True)
+    return dict_list
